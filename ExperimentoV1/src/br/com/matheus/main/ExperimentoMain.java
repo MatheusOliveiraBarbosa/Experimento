e2f810cf6fd3d682478130c72f37fc24f1a2aa24
Matheus Oliveira <matheus_obarbosa@hotmail.com>
2017-06-26 01:39:07 -0300

    Leitor de arquivos JAVA, juntamente com comparação aos estados

--- ExperimentoV1/src/br/com/matheus/main/ExperimentoMain.java
+++ ExperimentoV1/src/br/com/matheus/main/ExperimentoMain.java
@@ -1,69 +1,86 @@
 package br.com.matheus.main;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import br.com.matheus.models.State;
 import br.com.matheus.models.Transiction;
+import br.com.matheus.readerXML.ReaderXML;
 
 public class ExperimentoMain {
 
+	static ArrayList<State> states = new ArrayList<State>();
+	static ArrayList<Transiction> transictions = new ArrayList<Transiction>();
+	static ReaderXML reader = new ReaderXML();
+
+	private static final String TAG_NAME = "mxCell";
+	private static final String PATH_XML = "./data/status_transitions.xml";
+
 	public static void main(String[] args) {
 
-		ArrayList<State> states = new ArrayList<State>();
-		ArrayList<Transiction> transictions = new ArrayList<Transiction>();
+		// Ler o arquivo xml da Máquina de Estado
+		reader.readXML(PATH_XML);
+		// Cria uma lista de nodes com os elementos marcados por a tag
+		NodeList nList = reader.getNodeList(TAG_NAME);
 
-		try {
+		for (int i = 0; i < nList.getLength(); i++) {
 
-			File xmlStateMachine = new File("./data/status_transitions.xml");
-			System.out.println(xmlStateMachine.exists());
+			Node node = nList.item(i);
 
-			DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document xml = dBuilder.parse(xmlStateMachine);
-			xml.getDocumentElement().normalize();
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element) node;
+				if (element.hasAttribute("value")) {
+					if (!(element.getAttribute("value").equals(""))) {
+						if (element.getAttribute("value").contains("<br>")) {
+							State state = new State(element.getAttribute("id"),
+									element.getAttribute("value").replace("<br>", ""), element.getAttribute("parent"));
+							states.add(state);
+							System.out.println(state);
 
-			NodeList nList = xml.getElementsByTagName("mxCell");
-			for (int i = 0; i < nList.getLength(); i++) {
-
-				Node node = nList.item(i);
-
-				if (node.getNodeType() == Node.ELEMENT_NODE) {
-					Element element = (Element) node;
-					if (element.hasAttribute("value")) {
-						if (!(element.getAttribute("value").equals(""))) {
+						} else {
 							State state = new State(element.getAttribute("id"), element.getAttribute("value"),
 									element.getAttribute("parent"));
 							System.out.println(state);
 							states.add(state);
 						}
-					} else if (element.hasAttribute("target") && element.hasAttribute("source")) {
-						Transiction transiction = new Transiction(element.getAttribute("id"),
-								element.getAttribute("targer"), element.getAttribute("source"));
-						transictions.add(transiction);
-						System.out.println(transiction);
 					}
+				}
+				if (element.hasAttribute("target") && element.hasAttribute("source")) {
+					Transiction transiction = new Transiction(element.getAttribute("id"),
+							element.getAttribute("target"), element.getAttribute("source"));
+					transictions.add(transiction);
+					System.out.println(transiction);
+				}
 
+			}
+		}
+		System.out.println("States: " + states.size());
+		System.out.println("Transictions: " + transictions.size());
+		
+		//Ler o arquivo Java e verifica se contem valor associado ao estado
+		try {
+			File java = new File("./data/ThingStatus.java");
+			Scanner scanner = new Scanner(java);
+			while(scanner.hasNext()){
+				String line = scanner.nextLine();
+				for(State state : states){
+					if (line.contains(state.getValue())) {
+						System.out.println("consegui");
+					}
 				}
 			}
-
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+			
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+
 	}
 
 }
---
+++ ExperimentoV1/src/br/com/matheus/readerXML/ReaderXML.java
@@ -0,0 +1,54 @@
+package br.com.matheus.readerXML;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ReaderXML {
+
+	private Document xml = null;
+
+	public ReaderXML() {
+	}
+
+	public Document readXML(String path) {
+		// "./data/status_transitions.xml"
+		File xmlStateMachine = new File(path);
+		try {
+			DocumentBuilder dBuilder;
+			dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			xml = dBuilder.parse(xmlStateMachine);
+			xml.getDocumentElement().normalize();
+		} catch (ParserConfigurationException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		} catch (SAXException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return xml;
+	}
+	
+	public NodeList getNodeList(String tagName){
+		return xml.getElementsByTagName(tagName);
+	}
+
+	public Document getXml() {
+		return xml;
+	}
+
+	public void setXml(Document xml) {
+		this.xml = xml;
+	}
+
+}
